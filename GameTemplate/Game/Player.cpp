4e34c9243615db17c3535c2b88f23cb0a491f64f
azuma <1>
2021-02-23 16:51:53 +0900

    //プレイヤーのNのモデルを実装。 //青もしたかったけどなんかエラー出た。ddsファイルへの変換か読み込みがミスってると思う。 //そもそも画像からddsへの変換ができずに（jpegのせい？）、以前使ったddsを使用した。

--- GameTemplate/Game/Player.cpp
+++ GameTemplate/Game/Player.cpp
@@ -5,11 +5,11 @@
 
 bool Player::Start()
 {
-	//アニメーションクリップをロードする。
-	m_animationClips[enAnimClip_Idle].Load("Assets/animData/idle.tka");
-	m_animationClips[enAnimClip_Idle].SetLoopFlag(true);
-	m_animationClips[enAnimClip_Run].Load("Assets/animData/walk.tka");
-	m_animationClips[enAnimClip_Run].SetLoopFlag(true);
+	////アニメーションクリップをロードする。
+	//m_animationClips[enAnimClip_Idle].Load("Assets/animData/idle.tka");
+	//m_animationClips[enAnimClip_Idle].SetLoopFlag(true);
+	//m_animationClips[enAnimClip_Run].Load("Assets/animData/walk.tka");
+	//m_animationClips[enAnimClip_Run].SetLoopFlag(true);
 
 	m_charaCon.Init(40.0f, 100.0f, g_vec3Zero);
 	m_movePower = { 0.0f,0.0f,0.0f };
@@ -17,11 +17,11 @@ bool Player::Start()
 	//SkinModelRenderをNewGO。
 	m_skinModelRender = NewGO<SkinModelRender>(0);
 	//tkmファイルをロード。
-	m_skinModelRender->SetFileNametkm("Assets/modelData/unityChan.tkm");
+	m_skinModelRender->SetFileNametkm("Assets/modelData/Player_N.tkm");
 	//tksファイルをロード。
-	m_skinModelRender->SetFileNametks("Assets/modelData/unityChan.tks");
+	m_skinModelRender->SetFileNametks("Assets/modelData/Player_N.tks");
 	//アニメーションを設定。
-	m_skinModelRender->InitAnimation(m_animationClips, enAnimClip_Num);
+	//m_skinModelRender->InitAnimation(m_animationClips, enAnimClip_Num);
 	//最終的な初期化。
 	m_skinModelRender->Init(true, false);
 
