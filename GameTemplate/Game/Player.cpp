1a31649b2a4825b110151dbf13bb7f4d7e82ce02
bm20b04 <kbc20b04@stu.kawahara.ac.jp>
2021-03-18 12:36:59 +0900

    マージ用にコミットしたい

--- GameTemplate/Game/Player.cpp
+++ GameTemplate/Game/Player.cpp
@@ -3,78 +3,228 @@
 #include "DirectionLight.h"
 #include "MainCamera.h"
 
+#include "Key.h"
+
+
 bool Player::Start()
 {
-	////アニメーションクリップをロードする。
+
+
+	///アニメーションクリップをロードする。
 	//m_animationClips[enAnimClip_Idle].Load("Assets/animData/idle.tka");
 	//m_animationClips[enAnimClip_Idle].SetLoopFlag(true);
 	//m_animationClips[enAnimClip_Run].Load("Assets/animData/walk.tka");
 	//m_animationClips[enAnimClip_Run].SetLoopFlag(true);
 
-	m_charaCon.Init(100.0f, 200.0f, m_pos);
-	//m_movePower = { 0.0f,0.0f,0.0f };
+	//キャラコンの初期化。
+	m_charaCon.Init(50.0f, 50.0f, m_pos);
+
+	//プレイヤーのtkmとtksをロードする種。
 	const char* tkmFilePaths[] = {
-		"Assets/modelData/tkm/Player_N.tkm",
-		"Assets/modelData/tkm/Player_S.tkm"
+		"Assets/modelData/tkm/RedBall.tkm",
+		"Assets/modelData/tkm/BlueBall.tkm"
 	};
-	const char* tksFilePaths[] = {
+	/*const char* tksFilePaths[] = {
 		"Assets/modelData/tkm/Player_N.tks",
 		"Assets/modelData/tkm/Player_S.tks"
-	};
+	};*/
+
+	//プレイヤーの初期化。
 	for (int i = 0; i < enPlayer_Num; i++) {
 		//SkinModelRenderをNewGO。
 		m_skinModelRender[i] = NewGO<SkinModelRender>(1);
 		//tkmファイルをロード。
 		m_skinModelRender[i]->SetFileNametkm(tkmFilePaths[i]);
 		//tksファイルをロード。
-		m_skinModelRender[i]->SetFileNametks(tksFilePaths[i]);
+		//m_skinModelRender[i]->SetFileNametks(tksFilePaths[i]);
 		m_skinModelRender[i]->Init(true, false);
 		if (i == enPlayer_1) {
-			//プレイヤー1は最初は非アクティブ。
+			//Sは最初は非アクティブ。
 			m_skinModelRender[i]->Deactivate();
 		}
 	}
-	
+
 
 
 		//アニメーションを設定。
 	//m_skinModelRender->InitAnimation(m_animationClips, enAnimClip_Num);
-	//最終的な初期化。
 
+	m_rotation.SetRotationDeg(g_vec3AxisY, rot);
+
+	//座標を登録。
 	for (int i = 0; i < enPlayer_Num; i++) {
 		m_skinModelRender[i]->SetPosition(m_pos);
+		m_skinModelRender[i]->SetRotation(m_rotation);
 	}
 
 	return true;
 }
+
+
+Player::~Player()
+{
+}
+
+
+
 void Player::Update()
 {
-	//Vector3 moveSpeed;
-	//moveSpeed.x = g_pad[0]->GetLStickXF() * -3.0f;
-	//moveSpeed.z = g_pad[0]->GetLStickYF() * -3.0f;
-	//m_pos = m_charaCon.Execute(moveSpeed, 1.0f);
-	//m_skinModelRender->SetPosition(m_pos);
-	m_movePower.y -= 0.1f;
-	m_pos = m_charaCon.Execute(m_movePower, 1.0f);
+	//リスポーンした瞬間(3秒)の間は行われない処理。
+	//リスポーンしたときに動かないようにするため。
+	if (RespornFlag == false) {
 
-	//Vector3 samplePos = m_pos;
-	//samplePos.y += 100.0f;
-	for (int i = 0; i < enPlayer_Num; i++) {
-		m_skinModelRender[i]->SetPosition(m_pos);
+		//回転量を加算
+		rot1 -= 0.01f;
+
+		m_rotation1.SetRotationDeg(g_vec3AxisZ, rot1);
+		m_rotation.Multiply(m_rotation1);
+
+		//重力を設定。
+		m_movePower.y -= 0.2f;
+
+		//z方向には動かない。
+		m_movePower.z = 0.0f;
+
+
+		//座標を設定。
+		m_pos = m_charaCon.Execute(m_movePower, 1.0f);
+
+		//壁に当たっているなら
+		if (m_charaCon.IsOnWall()) {
+
+			//1/2の力で跳ね返る。
+			m_movePower.x *= -0.5f;
+		}
+
+		//地面上にいるなら
+		if (m_charaCon.IsOnGround()) {
+
+			//右に動いてたら
+			if (m_movePower.x >= 0.0f) {
+				//摩擦。
+				m_movePower.x -= 0.02f;
+				//もし減らしすぎたら０にする。
+				if (m_movePower.x < 0.0f) {
+					m_movePower.x = 0.0f;
+				}
+			}//左に動いてたら
+			else {
+				//摩擦。
+				m_movePower.x += 0.02f;
+				//もし増やしすぎたら０にする。
+				if (m_movePower.x > 0.0f) {
+					m_movePower.x = 0.0f;
+				}
+			}
+
+		}
+	}
+	//リスポーンフラグがtrueの時、
+	else {
+		//RespornCount変数を加算(タイマーの役割)
+		RespornCount++;
+		//もし3秒たったら、
+		if (RespornCount == 180) {
+			//RespornFlagをfalseにしてelseを抜ける。
+			RespornFlag = false;
+			//カウントを0(初期値)に戻す。
+			RespornCount = 0;
+		}
 	}
 
-	//Aボタンでプレイヤーの磁力を反転させる
-	if (g_pad[0]->IsTrigger(enButtonA)) {
-		ChangeState();
-		for (int i = 0; i < enPlayer_Num; i++) {
-			if (m_skinModelRender[i]->IsActive() == true) {
-				m_skinModelRender[i]->Deactivate();
+	//リスポーン!!!!!!!!
+
+	//Bボタンを連続で押されないようにするPushFlag
+	if (PushBFlag == true) {
+		//Bボタンでプレイヤーの位置を初期値に戻したい
+		if (g_pad[0]->IsTrigger(enButtonB)) {
+			//鍵の情報もらいます。
+			Key* key = FindGO<Key>("key");
+			//鍵をゲットしていなかったらスタート位置にリスポーンする。
+			if (key->getKeyFlg == false) {
+				//座標の移動
+				m_pos = { 300.0f,300.0f,0.0f };
 			}
+			//鍵をゲットしていたら鍵の場所にリスポーンする。
 			else {
-				m_skinModelRender[i]->Activate();
+				//鍵があった座標をプレイヤーの座標変数に代入。
+				m_pos = key->m_keyPos;
+			}
+			//キャラコンの初期化。
+			m_charaCon.Init(50.0f, 50.0f, m_pos);
+			//動く力の初期化
+			m_movePower = { 0.0f,0.0f,0.0f };
+
+			//もしプレイヤーがSのときリスポーンしたなら、
+			if (m_skinModelRender[1]->IsActive() == true) {
+
+				//プレイヤーをSからNに変換する。
+
+				ChangeState();
+
+				//アクティブフラグを更新。
+				for (int i = 0; i < enPlayer_Num; i++) {
+					if (m_skinModelRender[i]->IsActive() == true) {
+						m_skinModelRender[i]->Deactivate();
+					}
+					else {
+						m_skinModelRender[i]->Activate();
+					}
+				}
+			}
+
+			//リスポーンしたときに動かないようにする
+			RespornFlag = true;
+
+			//falseにすることでBボタンを押しても処理しないようにする。
+			PushBFlag = false;
+		}
+	}
+	//PushBFlag == falseのとき、
+	else {
+		//タイマー加算
+		PushBCount++;
+		//3秒経過すると、
+		if (PushBCount == 180) {
+			//タイマーの初期化
+			PushBCount = 0;
+			//Bボタンが押せる状態に戻す。
+			PushBFlag = true;
+		}
+	}
+
+
+	//座標を登録。
+	for (int i = 0; i < enPlayer_Num; i++) {
+
+		m_skinModelRender[i]->SetPosition(m_pos);
+
+		m_skinModelRender[i]->SetRotation(m_rotation);
+	}
+
+	//リスポーンした瞬間(3秒)の間は行われない処理。
+	//リスポーンしたときにプレイヤーをSに変えられないようにするため。
+	if (RespornFlag == false) {
+		//Aボタンでプレイヤーの磁力を反転させる
+		if (g_pad[0]->IsTrigger(enButtonA)) {
+			ChangeState();
+
+			//アクティブフラグを更新。
+			for (int i = 0; i < enPlayer_Num; i++) {
+				if (m_skinModelRender[i]->IsActive() == true) {
+					m_skinModelRender[i]->Deactivate();
+				}
+				else {
+					m_skinModelRender[i]->Activate();
+				}
 			}
 		}
 	}
+
+
+
+
+
 }
 
 void Player::ChangeState() {
